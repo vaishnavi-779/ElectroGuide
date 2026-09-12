7db034b54f1f5956fd5fac9c89e841172690597a
Vaaishnavi N P <paschapurvishal@gmail.com>
2026-09-12 21:19:34 +0530

    Add verified component image corrections

---
+++ component-images.js
@@ -0,0 +1,16 @@
+const electroGuideVerifiedImages={
+"trimmer":"https://commons.wikimedia.org/wiki/Special:FilePath/Siemens_Rel_37_D_94a-5_-_Dralowid_2k_Trimmer_Potentiometer-0682.jpg?width=900",
+"potentiometer":"https://commons.wikimedia.org/wiki/Special:FilePath/Potentiometer.jpg?width=900",
+"rheostat":"https://commons.wikimedia.org/wiki/Special:FilePath/Rheostat.jpg?width=900",
+"ferrite-bead":"https://commons.wikimedia.org/wiki/Special:FilePath/Ferrite_bead.jpg?width=900",
+"crystal":"https://commons.wikimedia.org/wiki/Special:FilePath/Crystal_Oscillator_DIP_Module_Teardown.png?width=900",
+"ntc-thermistor":"https://commons.wikimedia.org/wiki/Special:FilePath/NTC_Thermistor.jpg?width=900",
+"ceramic-capacitor":"https://commons.wikimedia.org/wiki/Special:FilePath/Electronic-Component-Ceramic-Capacitor.jpg?width=900",
+"electrolytic-capacitor":"https://commons.wikimedia.org/wiki/Special:FilePath/Electrolytic_capacitor.jpg?width=900",
+"film-capacitor":"https://commons.wikimedia.org/wiki/Special:FilePath/Emerson_Network_Power_7001232-J200_-_Film_capacitor_KNB1560_MKP-92806.jpg?width=900",
+"avalanche-diode":"https://commons.wikimedia.org/wiki/Special:FilePath/D25A10.jpg?width=900",
+"tv-s-diode":"https://commons.wikimedia.org/wiki/Special:FilePath/Orckit_ORfast-R2-A-SA-U-W_-_board_2_-_Semtech_LCDA05-2673.jpg?width=900",
+"ir-sensor":"https://commons.wikimedia.org/wiki/Special:FilePath/IR_Sensor.jpg?width=900",
+"idr":"https://commons.wikimedia.org/wiki/Special:FilePath/IR_Sensor.jpg?width=900"
+};
+Object.assign(electroGuideImageMap,electroGuideVerifiedImages);
